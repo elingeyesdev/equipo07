034b325a71a8e249a037cb5c92ef71059229d359
Lilpopyami12 <reckewegfarooq@gmail.com>
2026-05-26 13:34:51 -0400

    Restore original machinery wizard form

--- resources/views/maquinarias/create.blade.php
+++ resources/views/maquinarias/create.blade.php
@@ -2,9 +2,9 @@
 @section('hide_page_title', true)
 
 @section('content')
-    <div class="agro-wizard-page">
+    <div class="maquinaria-wizard-page">
         @if ($errors->any())
-            <div class="alert alert-danger agro-wizard-page__errors">
+            <div class="alert alert-danger maquinaria-wizard-page__errors">
                 <ul class="mb-0">
                     @foreach ($errors->all() as $e)
                         <li>{{ $e }}</li>
